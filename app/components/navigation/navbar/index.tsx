0b1d1941d6b7ed5ca40002fd57af70f3e22caace
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-08-04 00:08:43 +0300

    fixed styles

--- app/components/navigation/navbar/index.tsx
+++ app/components/navigation/navbar/index.tsx
@@ -7,26 +7,28 @@ import styles from "./index.module.css";
 const Navbar = () => {
   return (
     <>
-      <div className={styles.container}>
-        <Logo />
-        <ul className={styles.navbar}>
-          <li>
-            <Link href="/about">
-              <p>About Us</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/services">
-              <p>Services</p>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contacts">
-              <p>Contacts</p>
-            </Link>
-          </li>
-        </ul>
-        <Button />
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <Logo />
+          <ul className={styles.navbar}>
+            <li>
+              <Link href="/about">
+                <p>About Us</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/services">
+                <p>Services</p>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contacts">
+                <p>Contacts</p>
+              </Link>
+            </li>
+          </ul>
+          <Button />
+        </div>
       </div>
     </>
   );
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -2,7 +2,54 @@ import React from "react";
 import styles from "./page.module.css";
 
 const Contacts = () => {
-  return <div className={styles.container}>Contacts</div>;
+  return (
+    <div className={styles.container}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat deleniti
+      impedit fugit asperiores est atque unde culpa, quod architecto optio,
+      sapiente quasi doloremque, quia pariatur repellendus laborum ratione sed
+      illo a provident laudantium incidunt quas delectus. Tempora quasi
+      consectetur molestias tempore rerum rem perspiciatis sapiente sed quo
+      voluptates assumenda suscipit nobis enim, perferendis illum, esse ad,
+      beatae consequuntur cupiditate ab impedit. Iusto doloremque, est sit
+      facere eveniet, aliquid perspiciatis libero fugiat ipsum corporis
+      reprehenderit aliquam reiciendis omnis dolorum blanditiis aspernatur
+      beatae nostrum atque, necessitatibus quasi pariatur quia iure animi. Sit
+      molestias dignissimos autem maxime eaque amet non tempore vitae sunt,
+      nobis, sapiente, ducimus totam magni iusto. Culpa officia quo quae,
+      architecto in perferendis id vero sequi voluptatem minima doloremque iusto
+      commodi nisi error quibusdam repudiandae fugit totam dolor? Explicabo ad
+      nisi vel nostrum repellendus ducimus sed modi veritatis molestias quia
+      aliquam in temporibus molestiae eos odit, ab dolor dignissimos quasi
+      maiores! Delectus veniam dolor modi, iusto ex eius velit? Quod facere
+      rerum sit, nesciunt quasi modi accusamus veritatis odio non vel
+      perspiciatis deserunt optio similique rem tempora suscipit repudiandae
+      cum! Perferendis at eveniet quos fuga earum. Ducimus, repellendus
+      perferendis non explicabo reiciendis molestiae sit earum sint ab eaque
+      deserunt, pariatur veritatis reprehenderit voluptas autem voluptatibus eum
+      corporis molestias et, id ratione vel harum. Mollitia, ad minus
+      laboriosam, omnis facere illum quae velit earum iusto necessitatibus
+      doloribus sint repudiandae minima possimus eos! Enim, ad necessitatibus?
+      Illum laudantium repellendus iure veritatis fugiat. Voluptate, commodi.
+      Dolores consequuntur repudiandae, necessitatibus veritatis tempore
+      molestias, consequatur quam asperiores quasi, hic dignissimos qui! Illo
+      harum consequatur deserunt inventore eos nobis possimus hic delectus
+      tempore? Aspernatur, numquam! Eveniet recusandae a eaque, labore est
+      itaque veritatis? Ea voluptates quam eum saepe nemo autem officiis
+      molestias ipsa, placeat fugiat! Recusandae illo inventore tempore atque,
+      ipsa vitae eius suscipit nobis quas modi dignissimos omnis ipsum tenetur,
+      asperiores, animi facilis in praesentium. Accusantium nostrum animi
+      repellendus autem consectetur quibusdam enim laboriosam recusandae
+      commodi, exercitationem id inventore excepturi? Minima labore earum
+      reprehenderit, aspernatur ea minus provident vero reiciendis corporis
+      nemo, veritatis officia ab blanditiis laborum illum pariatur corrupti
+      assumenda. Dignissimos quo sed nihil totam inventore minima corrupti
+      molestias aperiam, deserunt vero eaque optio culpa, voluptatem
+      voluptatibus saepe quis aut odio laboriosam minus porro. Alias repellendus
+      quia ut hic quae tempore dolorum quis blanditiis exercitationem. Impedit
+      placeat dolor dolores provident eos modi tempore, temporibus deserunt
+      nemo, corrupti cupiditate. Omnis fuga sint nihil incidunt totam.
+    </div>
+  );
 };
 
 export default Contacts;
