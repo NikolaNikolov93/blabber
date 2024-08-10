88dbb72e7c293049657926ca251a74bf0eba80e1
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-08-10 10:23:46 +0300

    Initial firebase setup

--- app/recepies/page.tsx
+++ app/recepies/page.tsx
@@ -1,14 +1,35 @@
-import recepies from "@/public/recepies";
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { db } from "../../lib/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const Recepies = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "recepies"));
+        const recepies = [];
+        querySnapshot.forEach((doc) => {
+          recepies.push(doc.data());
+        });
+        setData(recepies);
+        console.log(recepies);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className={styles.wrapper}>
         <ul>
-          {recepies.map((recepie) => (
+          {data.map((recepie) => (
             <li key={recepie.name}>
               <Link href={`recepies/${recepie.name}`}>{recepie.name}</Link>
             </li>
